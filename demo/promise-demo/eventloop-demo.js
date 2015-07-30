7a35636bf0c7e31ee0e7072779ddf17ecb794c84
WaterBolik <WaterBolik@163.com>
2015-07-30 18:39:24 +0800

    promise demo

---
+++ demo/promise-demo/eventloop-demo.js
@@ -0,0 +1,83 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+// 要理解event loop首先需要了解的就是event driven programming（事件驱动的编程）。
+// 定义非常简单：event-driven programming就是程序的控制流程是由事件或者状态的改变决定的。
+// 主要的实现机制就是用一个中心控制台监听事件，并在事件发生的时候调用这个事件对应的回调函数（状态的改变也是一样）。
+function MyEmitter() {
+  EventEmitter.call(this);
+}
+util.inherits(MyEmitter, EventEmitter);
+MyEmitter.prototype.doStuff = function() {
+  console.log('---before');
+  this.emit('fire');
+  console.log('---after');
+}
+var me = new MyEmitter();
+me.on('fire', function(){
+  console.log('---emit fired');
+});
+me.doStuff();  // Output: ---before ---emit fired ---after
+// EventEmitter看起来是异步的，因为他总是被用来发出异步操作完成的信号。但是，EventEmitter API是完全同步的。
+// emit方法可能被异步调用，但是需要主要到全部的监听方法都是按照添加的顺序同步执行的。
+
+// Event Emitter和Event Loop
+// 为了简化和Event loop的互操作，所以有了EventEmitter。
+// 用EventEmitter可以很容易创建一个基于事件的API。下面我们就一些主要内容做讲解。
+// 下面的代码展示了没有同步发出事件会造成用户错过事件的情况：
+function MyThing1() {
+  EventEmitter.call(this);
+  this.emit('thing1');
+}
+util.inherits(MyThing1, EventEmitter);
+var mt1 = new MyThing1();
+mt1.on('thing1', function(){
+  // never going to happen.
+});
+// 以上代码的问题就在于‘thing1’永远不会被用户捕捉到，因为MyThing()必须在初始化完成之后才能监听事件。
+
+// 下面是一个简单地解决方案，不需要任何另外的闭包：
+function MyThing2() {
+  EventEmitter.call(this);
+  setImmediate(emitThing2, this);
+}
+util.inherits(MyThing2, EventEmitter);
+function emitThing2(self) {
+  self.emit('thing2');
+}
+var mt2 = new MyThing2();
+mt2.on('thing2', function(){
+  console.log('bravo, thing2 captured.');
+});
+
+// 下面的代码页可以运行，只不过消耗较多。
+function MyThing3() {
+  EventEmitter.call(this);
+  setImmediate(this.emit.bind(this, 'thing3'));
+}
+util.inherits(MyThing3, EventEmitter);
+var mt3 = new MyThing3();
+mt3.on('thing3', function(){
+  // bravo
+  console.log('bravo, thing3 captured.');
+});
+
+// 另一种情况是触发错误。查出你的应用的问题非常麻烦，而且如果没有调用栈的话，简直无法排查。
+// 一个Error在异步执行的深处初始化的时候可能会导致调用栈丢失。解决这个问题最靠谱的两个办法就是同步emit事件，
+// 或者确保Error带了足够的相关信息。请看以下代码的演示：
+// MyThing3.prototype.foo = function () {
+//   var er = doFirstThing();
+//   if (er) {
+//     // emit error asynchronously
+//     setImmediate(emitError, this, new Error('Bad stuff'));
+//     return;
+//   }
+//   // emit error synchronously
+//   var er = doSecondThing();
+//   if (er) {
+//     this.emit('error', 'More bad stuff');
+//     return;
+//   }
+// };
+// mt3.foo();
+// emit的错误应该立即被处理，以防程序继续执行。而且在构造函数中emit错误也不是个好主意。
---
+++ demo/promise-demo/readfile-demo.js
@@ -0,0 +1,63 @@
+
+// Promise是异步代码实现控制流的一种方式。这一方式可以让你的代码干净、可读并且健壮。
+
+var Promise = require('bluebird');
+// bluebird提供了一个非常有用的功能来promise化不返回promise的模块。
+// 比如，promise化fs模块，只需要简单地require bluebird模块和一个被promise化的fs模块。
+var fs = Promise.promisifyAll(require('fs'));
+
+// 我想要异步的读取一个文件，然后创建一个目录并创建一个文件。
+// 你可以看到这个简单的三步走的任务变成了多么丑陋的嵌套的代码，
+// 尤其是一旦你再在这些代码中添加逻辑控制的时候，代码更是不可想象丑陋。
+fs.readFile('directory/file-to-read', function(err, file){
+  if (err){
+    //handle error
+    console.log(err);
+  } else {
+    //do something with the file
+    fs.mkdir('directory/new-directory', function(err, file){
+      if (err) {
+        //handle error
+        console.log(err);
+      } else {
+        //new directory has been made
+        fs.writeFile('directory/new-directory/message.txt', function(err, file){
+          if(err) {
+            // handle error
+            console.log(err);
+          } else {
+            // File successfully created
+          }
+        });
+      }
+    });
+  }
+});
+
+// 以上面对的代码作为例子，我们将探究如何用Promise解决上面说到的问题
+fs.readFileAsync('directory/file-to-read')
+  .then(function(fileData){
+    return fs.mkdirAsync('directory/new-directory');
+  })
+  .then(function(){
+    return fs.writeFileAsync('directory/new-directory/message.txt');
+  })
+  .catch(function(err){
+    //do something with the error and handle it
+    console.log(err);
+  });
+
+// 创建promise需要提供resolve和reject的回调方法。每一个都需要传对了：
+//myPromise.js
+var Promise = require('bluebird');
+module.exports = function(){
+  return new Promise(function(resolve, reject){
+    tradiationCallbackBasedThing(function(error, data){
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data)
+      }
+    });
+  });
+}
