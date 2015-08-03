2263d2b8852aaaa2cc0d358ae2e57d31ac9e0e54
WaterBolik <WaterBolik@163.com>
2015-08-03 17:47:44 +0800

    grunt demo

--- demo/grunt-demo/Gruntfile.js
+++ demo/grunt-demo/Gruntfile.js
@@ -19,6 +19,18 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js',
         dest: 'Gruntfile.min.js'
       }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
   });
 
@@ -29,17 +41,48 @@ module.exports = function(grunt) {
   // 都可以在Gruntfile中以简单命令的形式使用：
   // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // 通过定义 default 任务，可以让Grunt默认执行一个或多个任务。
   // 在下面的这个案例中，执行 grunt 命令时如果不指定一个任务的话，将会执行uglify任务。
   // 这和执行grunt uglify 或者 grunt default的效果一样。
   // default任务列表数组中可以指定任意数目的任务（可以带参数）。
   // 默认被执行的任务列表。
-  grunt.registerTask('default', ['uglify']);
+  // grunt.registerTask(taskName, [description, ] taskList)
+  // grunt.registerMultiTask(taskName, [description, ] taskFunction)
+  grunt.registerTask('default', ['jshint', 'uglify']);
 
   // A very basic default task.
-  // grunt.registerTask('default', 'Log some stuff.', function() {
-  //   grunt.log.write('Logging some stuff...').ok();
-  // });
+  // 你可以和任务一起疯狂。如果你的任务并没有遵循 "多任务" 结构，那就使用自定义任务。
+  grunt.registerTask('bar', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...').ok();
+  });
+
+  // 在一个任务内部，你可以执行其他的任务。
+  grunt.registerTask('foo', 'My "foo" task.', function() {
+    // Enqueue "bar" and "baz" tasks, to run after "foo" finishes, in-order.
+    grunt.task.run('bar', 'baz');
+    // Or:
+    grunt.task.run(['bar', 'baz']);
+  });
+
+  // 任务也可以是异步的。
+  grunt.registerTask('asyncfoo', 'My "asyncfoo" task.', function() {
+    // Force task into async mode and grab a handle to the "done" function.
+    var done = this.async();
+    // Run some sync stuff.
+    grunt.log.writeln('Processing task...');
+    // And some async stuff.
+    setTimeout(function() {
+      grunt.log.writeln('All done!');
+      done();
+    }, 1000);
+  });
+
+  // 任务也可以访问它们自身名称和参数。
+  grunt.registerTask('fooo', 'My "fooo" task.', function(a, b) {
+    grunt.log.writeln(this.name, a, b);
+  });
 
 };
