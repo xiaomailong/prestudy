07744d370cca66277a3e168b98119b3c6bc800fd
WaterBolik <WaterBolik@163.com>
2015-08-13 11:45:25 +0800

    React Native 简介：用 JavaScript 搭建 iOS 应用(1)

---
+++ demo/react_native_demo/Search.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  View,
+  Text,
+  Component
+} = React;
+
+var styles = StyleSheet.create({
+  description: {
+    fontSize: 20,
+    backgroundColor: 'white'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+class Search extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search Tab
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.exports = Search;
--- demo/react_native_demo/index.ios.js
+++ demo/react_native_demo/index.ios.js
@@ -1,61 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
+var Featured = require('./Featured');
+var Search = require('./Search');
+
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  TabBarIOS,
+  Component
 } = React;
 
-var react_native_demo = React.createClass({
-  render: function() {
+class react_native_demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'featured'
+    };
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-            To get started, edit index.ios.js.
-        </Text>
-        <Text style={styles.hello}>
-          Hello react native!
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'featured'}
+          icon={{uri:'featured'}}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'featured'
+            });
+          }}>
+          <Featured/>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'search'}
+          icon={{uri:'search'}}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'search'
+            });
+          }}>
+          <Search/>
+        </TabBarIOS.Item>
+      </TabBarIOS>
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  hello: {
-    textAlign: 'center',
-    color: '#ee00ee',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('react_native_demo', () => react_native_demo);
