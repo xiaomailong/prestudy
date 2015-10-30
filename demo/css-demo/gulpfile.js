21341b721fe064f4ad0d647b3ad750e10c29216d
WaterBolik <WaterBolik@163.com>
2015-10-30 12:01:20 +0800

    sass scss

--- demo/css-demo/gulpfile.js
+++ demo/css-demo/gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 
+// 合并文件
+concat = require('gulp-concat');
+
 // 自动增加浏览器识别前缀
 var autoprefixer = require('autoprefixer');
 
@@ -42,11 +45,14 @@ var options = {
   legacy: false // fixes the double-margin bug in older browsers. Defaults to false
 };
 
+// 样式向导
+var styleGuide = require('postcss-style-guide');
+var fs = require('fs');
+var processedCSS = fs.readFileSync('./css/all.css', 'utf-8');
+
 var processors = [
   grid(options),
-  autoprefixer({
-    browsers: 'safari >= 9, ie >= 11'
-  }),
+  autoprefixer,
   cssnext,
   precss,
   will_change,
@@ -56,44 +62,33 @@ var processors = [
   vmin,
   pixrem,
   atImport,
-  mqpacker,
-  cssnano
+  styleGuide({
+    name: "css-demo", // Project name
+    theme: "default", // Theme name: default, sassline, 1column, forest
+    file: "styleguide.html", // Style guide file name (default: styleguide.html)
+    dir: "docs", // Output directory (default: "docs")
+    showCode: true, // The flag to show CSS code (default: true)
+    processedCSS: processedCSS
+  }),
+  // mqpacker,
+  // cssnano,
 ];
 
 gulp.task('css', function () {
-  return gulp.src('./src/*.css')
+  return gulp.src('./_css/*.css')
     .pipe(postcss(processors))
-    // .pipe(nano())
+    .pipe(concat('all.css'))
+    .pipe(nano())
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./src/*.sass')
+  return gulp.src('./_scss/*.scss')
     .pipe(sass()
       .on('error', sass.logError))
+    .pipe(gulp.dest('./_css'))
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./css'));
-});
-
-
-// 样式向导
-var styleGuide = require('postcss-style-guide');
-
-var fs = require('fs');
-
-gulp.task('style-guide', function () {
-  var processedCSS = fs.readFileSync('./css/diamond1.css', 'utf-8');
-  var processors = [
-    styleGuide({
-      name: "css-demo", // Project name
-      theme: "default", // Theme name: default, sassline, 1column, forest
-      file: "styleguide.html", // Style guide file name (default: styleguide.html)
-      dir: "docs", // Output directory (default: "docs")
-      showCode: true, // The flag to show CSS code (default: true)
-      processedCSS: processedCSS
-    })
-  ];
-  return gulp.src('./src/diamond1.css')
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('./css'));
+    .pipe(concat('all.css'))
+    .pipe(nano())
+    .pipe(gulp.dest('./css'));;
 });
