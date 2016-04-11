58651a4919a7f280010e02cdd6d0e0f4fb39df57
WaterBolik <WaterBolik@163.com>
2016-04-11 17:36:40 +0800

    meteor 1.3.1

--- shell-src/install.sh
+++ shell-src/install.sh
@@ -3,6 +3,9 @@
 # JavaScript Web云平台OS.js
 curl -sS http://os.js.org/installer | sh
 
+# Meteor —— Full stack JavaScript for amazing apps
+curl https://install.meteor.com | /bin/sh
+
 # Homebrew OS X 不可或缺的套件管理器
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # curl -LsSf http://github.com/mxcl/homebrew/tarball/master | sudo tar xvz -C/usr/local --strip 1
