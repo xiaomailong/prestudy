ff892e5712b69daf20b1e7e6fec696df6a619c9c
BOLIK-X1\Bolik <Bolik@163.com>
2016-09-02 17:19:43 +0800

    Svn Apm Update

---
+++ shell-src/Windows/ApmInstall.cmd
@@ -0,0 +1,132 @@
+@echo off
+
+apm install activate-power-mode
+
+REM 快速打开、切换文件
+apm install advanced-open-file
+
+REM 代码格式化 美化代码，格式化代码的插件。
+apm install atom-beautify
+
+REM 代码提示
+apm install atom-ctags
+
+REM html预览
+apm install atom-html-preview
+
+REM JS代码智能提示补全
+apm install atom-ternjs
+REM typescript
+apm install atom-typescript
+REM git 可视化
+apm install atomatigit
+REM Web前端
+apm install autoclose-html
+REM 目录自动完成 输入.或者/的时候，会自动提示路径和路径下的文件。非常好用！
+apm install autocomplete-paths
+REM autoprefixer:css样式的兼容性添加。当写好自己的css之后，会自动添加类似-webkit-等前缀的css样式代码。
+apm install autoprefixer
+
+REM color-picker:css中需要赋值颜色的时候，使用这个插件可以看到一个完整的取色器，实时的取色，改变。。。非常方便！
+apm install color-picker
+
+REM 代码提示
+apm install css-snippets
+REM jsdoc 给js添加注释
+apm install docblockr
+REM 提高开发效率
+apm install emmet
+REM Atom装了expose这个插件之后，mac用户使用shift＋command＋e组合键可以得到意想不到的酷炫！
+apm install expose
+
+REM file-icons:左边工程目录的每一个文件，如果有一个小图标表示就漂亮了，没错，就需要这个插件了。
+apm install file-icons
+
+REM apm install grunt-runner
+
+apm install git-plus
+
+apm install html-to-string
+
+apm install jquery-snippets
+apm install javascript-snippets
+
+apm install language-babel
+apm install language-diff
+apm install language-docker
+apm install language-javascript-jsx
+apm install language-powershell
+apm install language-batch
+apm install language-unix-shell
+
+REM 代码校验 自动提示你代码中不规范的地方，让你拥有更加规范的代码习惯。如果你想得到完整的信息提示，则推荐这个插件linter-jshint
+apm install linter
+apm install linter-csslint
+apm install linter-eslint
+apm install linter-jscs
+apm install linter-jshint
+apm install linter-tslint
+apm install linter-docker
+apm install linter-shellcheck
+REM markdown-format，markdown-writer：欢使用markdown来写东西的话，那么你一定会爱上在Atom上写markdown的感觉~~
+apm install markdown-format
+apm install markdown-writer
+REM merge
+apm install merge-conflicts
+
+REM minimap: 缩略图
+apm install minimap
+apm install minimap-codeglance
+apm install minimap-find-and-replace
+apm install minimap-git-diff
+apm install minimap-highlight-selected
+apm install minimap-linter
+REM 放大镜的功能，这个插件就会让你的鼠标移动到缩略图上的时候放大显示那边部分的代码
+apm install minimap-codeglance
+REM 当你想替换单词的时候你会想起ctrl+D，通过这个插件你就可以在缩略图上看到所有你选中的字符串
+apm install minimap-find-and-replace
+REM 通过这个插件，每当你修改你的代码的时候你就会在缩略图上看到和之前git中的区别
+apm install minimap-git-diff
+REM 当你选中部分代码的时候，它就会高亮的出现在缩略图中
+apm install minimap-highlight-selected
+REM 这个插件让你的缩略图显示的更加漂亮和完整
+apm install minimap-linter
+
+REM Facebook专门为React开发的IDE
+apm install nuclide
+apm install nuclide-installer
+
+REM atom-terminal：适合 Mac OSX 10.9 （Mavericks） 及以下的操作系统；
+REM atomerinal：atom-terminal 在 Mac OSX Yosmite 和 EI Capitan 下不能准确定位路径，
+REM 这个插件是对 atom-terminal 的 fork 和修改，适合版本更高的操作系统；
+apm install atom-terminal
+apm install atomerinal
+REM 直接在atom里面写命令
+apm install terminal-panel
+
+REM 颜色显示插件
+apm install pigments
+
+apm install project-manager
+REM react
+apm install react
+REM SVG预览
+apm install svg-preview
+
+REM 主题插件
+apm install atom-gooey-syntax
+apm install atom-material-ui
+apm install atom-material-syntax
+apm install seti-ui
+apm install slack-ui
+apm install dracula-theme
+apm install nucleus-dark-ui
+apm install idiomatic-dark-syntax
+apm install batman-syntax
+apm install colorblind-syntax
+apm install native-ui
+
+echo    Press any key to exit ...
+Pause>nul
+echo on
+exit
--- shell-src/apm-install.sh
+++ shell-src/apm-install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# apm update
-# apm upgrade
-
-# markdown
 apm install activate-power-mode
+
 # 快速打开、切换文件
 apm install advanced-open-file
+
 # 代码格式化 美化代码，格式化代码的插件。
 apm install atom-beautify
+
 # 代码提示
 apm install atom-ctags
-# 主题
-# apm install atom-gooey-syntax
+
 # html预览
 apm install atom-html-preview
-
 
 # JS代码智能提示补全
 apm install atom-ternjs
@@ -29,13 +26,10 @@ apm install autoclose-html
 apm install autocomplete-paths
 # autoprefixer:css样式的兼容性添加。当写好自己的css之后，会自动添加类似-webkit-等前缀的css样式代码。
 apm install autoprefixer
-# batman语法主题
-# apm install batman-syntax
 
 # color-picker:css中需要赋值颜色的时候，使用这个插件可以看到一个完整的取色器，实时的取色，改变。。。非常方便！
 apm install color-picker
 
-# apm install colorblind-syntax
 # 代码提示
 apm install css-snippets
 # jsdoc 给js添加注释
@@ -54,13 +48,10 @@ apm install git-plus
 
 apm install html-to-string
 
-# apm install idiomatic-dark-syntax
-
 apm install jquery-snippets
 apm install javascript-snippets
 
-# apm install language-babel
-
+apm install language-babel
 apm install language-diff
 apm install language-docker
 apm install language-javascript-jsx
@@ -76,7 +67,7 @@ apm install linter-jscs
 apm install linter-jshint
 apm install linter-tslint
 apm install linter-docker
-# apm install linter-shellcheck
+apm install linter-shellcheck
 # markdown-format，markdown-writer：欢使用markdown来写东西的话，那么你一定会爱上在Atom上写markdown的感觉~~
 apm install markdown-format
 apm install markdown-writer
@@ -90,26 +81,28 @@ apm install minimap-find-and-replace
 apm install minimap-git-diff
 apm install minimap-highlight-selected
 apm install minimap-linter
-# minimap-codeglance	放大镜的功能，这个插件就会让你的鼠标移动到缩略图上的时候放大显示那边部分的代码
-# minimap-find-and-replace	当你想替换单词的时候你会想起ctrl+D，通过这个插件你就可以在缩略图上看到所有你选中的字符串
-# minimap-git-diff	通过这个插件，每当你修改你的代码的时候你就会在缩略图上看到和之前git中的区别
-# minimap-highlight-selected	当你选中部分代码的时候，它就会高亮的出现在缩略图中
-# minimap-linter	这个插件让你的缩略图显示的更加漂亮和完整
-
-
-# apm install native-ui
+# 放大镜的功能，这个插件就会让你的鼠标移动到缩略图上的时候放大显示那边部分的代码
+apm install minimap-codeglance
+# 当你想替换单词的时候你会想起ctrl+D，通过这个插件你就可以在缩略图上看到所有你选中的字符串
+apm install minimap-find-and-replace
+# 通过这个插件，每当你修改你的代码的时候你就会在缩略图上看到和之前git中的区别
+apm install minimap-git-diff
+# 当你选中部分代码的时候，它就会高亮的出现在缩略图中
+apm install minimap-highlight-selected
+# 这个插件让你的缩略图显示的更加漂亮和完整
+apm install minimap-linter
 
 # Facebook专门为React开发的IDE
 apm install nuclide
-# apm install nuclide-installer
+apm install nuclide-installer
 
 # atom-terminal：适合 Mac OSX 10.9 （Mavericks） 及以下的操作系统；
 # atomerinal：atom-terminal 在 Mac OSX Yosmite 和 EI Capitan 下不能准确定位路径，
 # 这个插件是对 atom-terminal 的 fork 和修改，适合版本更高的操作系统；
-# apm install atom-terminal
+apm install atom-terminal
 apm install atomerinal
-
-# apm install nucleus-dark-ui
+# 直接在atom里面写命令
+apm install terminal-panel
 
 # 颜色显示插件
 apm install pigments
@@ -117,16 +110,18 @@ apm install pigments
 apm install project-manager
 # react
 apm install react
-# 主题
 # SVG预览
 apm install svg-preview
-# 直接在atom里面写命令
-apm install terminal-panel
 
-# 主题插件 -----------
-# atom-material-ui:这是一个主题插件,他有自带的一些动态效果，特别酷炫。话不多说，自己试试就知道。
+# 主题插件
+apm install atom-gooey-syntax
 apm install atom-material-ui
 apm install atom-material-syntax
 apm install seti-ui
 apm install slack-ui
-apm install dracula-theme 
+apm install dracula-theme
+apm install nucleus-dark-ui
+apm install idiomatic-dark-syntax
+apm install batman-syntax
+apm install colorblind-syntax
+apm install native-ui
