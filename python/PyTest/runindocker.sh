4a3b46719794fb6af47ba76dceeed91fba0878ce
孔波利 <Bolik@163.com>
2017-01-10 17:37:44 +0800

    alpine python

---
+++ python/PyTest/runindocker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 进入python Dockerfile 目录
+# cd /Volumes/Work/OpenSource/WaterBolik/prestudy/docker/python 
+
+# 编译构建docker镜像
+# docker build -t bpython .
+
+# 执行docker
+docker run -it --rm  -v /Volumes/Work/OpenSource/WaterBolik/prestudy/python/PyTest:/pytest bpython 
