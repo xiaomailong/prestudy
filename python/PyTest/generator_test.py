95316731cce5e1ee4559d95aad6cddcfc6563bbf
BOLIK-X1CARBON\Bolik <Bolik@163.com>
2017-01-14 23:14:38 +0800

    python 迭代器

--- python/PyTest/generator_test.py
+++ python/PyTest/generator_test.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import pytest
+
+# Iterable、Iterator与Generator之间的关系
+# 生成器对象既是可迭代对象，也是生成器： 
+#   生成器不但可以作用与for循环，还可以被next()函数不断调用并返回下一个值，
+#   直到最后抛出StopIteration错误表示无法继续返回下一个值了。
+#   生成器同时满足可迭代对象和迭代器的定义；
+# 迭代器对象一定是可迭代对象，反之则不一定： 
+#   例如list、dict、str等集合数据类型是可迭代对象，但不是迭代器，但是它们可以通过iter()函数生成一个迭代器对象。
+# 也就是说：迭代器、生成器和可迭代对象都可以用for循环去迭代，生成器和迭代器还可以被next()方函数调用并返回下一个值。
+
 # 列表生成式 ----------------------------------------------------------------------
 # 基础：    [exp for iter_var in iterable]
 # 带过滤：  [exp for iter_var in iterable if_exp]
@@ -79,11 +90,11 @@ def test_list_generator():
 #   只记录当前的位置
 #   只能next，不能prev
 # 调用生成器产生新的元素，有两种方式：
-#   调用内置的next()方法
-#   使用循环对生成器对象进行遍历（推荐）
-#   调用生成器对象的send()方法
+#   调用内置的next()方法，以抛出一个StopIeration异常终止。
+#   使用循环对生成器对象进行遍历（推荐，不会抛出StopIeration异常）
+#   调用生成器对象的send()方法，可以给yield传值（第一次必须是None），其他功能同next()方法
 
-
+ret = ''
 def test_yield_generator():
     # 使用类似列表生成式的方式构造生成器
     g1 = (2 * n + 1 for n in range(3, 6))
@@ -96,13 +107,82 @@ def test_yield_generator():
     assert list(g1) == list(g2)
     # 使用next()方法遍历生成器
     for x1 in g1:
-        assert x1 == next(g2)
-    #
-    # print(next(g1)) 
+        assert x1 == next(g2)    
     # 生成器在语境中只能被循环调用一次
-    # for x2 in g2:
-    #     assert x2 == next(g1)
+    # 如果在调用会抛出StopIteration异常
+    with pytest.raises(StopIteration):
+        next(g1)
     
+    # 调用生成器对象的send()方法
+    #   next()会调用yield，但不给它传值
+    #   send()会调用yield，也会给它传值（该值将成为当前yield表达式的结果值）
+    # 注意：第一次调用生成器的send()方法时，参数只能为None，否则会抛出异常。
+    #      当然也可以在调用send()方法之前先调用一次next()方法，目的是让生成器先进入yield表达式。
+    def my_range3(start, end):
+        for n in range(start, end):
+            ret = yield 2*n + 1
+            print(ret)
 
+    g3 = my_range3(3, 6)
+    assert g3.send(None) == 7
+    # assert ret == ''
+    assert g3.send('hello01') == 9
+    # assert ret == 'hello01'
+    assert g3.send('hello02') == 11
+    # assert ret == 'hello02'
+    g4 = my_range3(3, 6)
+    assert next(g4) == 7
+    assert next(g4) == 9
+    assert next(g4) == 11
+    # assert ret == 'test'
+   
+    
+# List生成式与yield生成器区别
+# 列表生成式是直接创建一个新的list，它会一次性地把所有数据都存放到内存中，这会存在以下几个问题：
+#   内存容量有限，因此列表容量是有限的；
+#   当列表中的数据量很大时，会占用大量的内存空间，如果我们仅仅需要访问前面有限个元素时，就会造成内存资源的极大浪费；
+#   当数据量很大时，列表生成式的返回时间会很慢；
+# yield生成器中的元素是按照指定的算法推算出来的，只有调用时才生成相应的数据。
+# 这样就不必一次性地把所有数据都生成，从而节省了大量的内存空间，这使得其生成的元素个数几乎是没有限制的，
+# 并且操作的返回时间也是非常快速的（仅仅是创建一个变量而已）。
 
-#
+import time
+import sys
+def test_list_vs_yield():
+    time_start1 = time.time()
+    g1 = [x for x in range(10000000)]
+    time_end1 = time.time()
+    t1 = (time_end1 - time_start1)
+    m1 = sys.getsizeof(g1)
+    print('列表生成式返回结果花费的时间： %s' % t1)
+    print('列表生成式返回结果占用内存大小：%s' % m1)
+
+    def my_range(start, end):
+        for x in range(start, end):
+            yield x
+    time_start2 = time.time()
+    g2 = my_range(0, 10000000)
+    time_end2 = time.time()
+    t2 = time_end2 - time_start2
+    m2 = sys.getsizeof(g2)
+    print('生成器返回结果花费的时间： %s' % t2)
+    print('生成器返回结果占用内存大小：%s' % m2)
+    
+    assert t1 > t2 * 100000
+    assert m1 > m2 * 900000
+
+from collections import Iterable
+from collections import Iterator
+# import collections
+# Python中的Iterator对象表示的是一个数据流，Iterator可以被next()函数调用被不断返回下一个数据，
+# 直到没有数据可以返回时抛出StopIteration异常错误。
+# 可以把这个数据流看做一个有序序列，但我们无法提前知道这个序列的长度。
+# 同时，Iterator的计算是惰性的，只有通过next()函数时才会计算并返回下一个数据。
+def test_iterator():
+    assert isinstance([], Iterable)
+    assert isinstance((x for x in range(5)), Iterator)
+    def my_range(start, end):
+        for x in range(start, end):
+            yield x
+    g = my_range(0, 100)
+    assert isinstance(g, Iterator)
--- python/PyTest/pytest_test.py
+++ python/PyTest/pytest_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import pytest
+
 # 编写 pytest 测试样例需要按照下面的规则：
 #     测试文件以 test_ 开头（以 _test 结尾也可以）
 #     测试类以 Test 开头，并且不能带有 __init__ 方法
@@ -9,15 +11,143 @@
 
 def func(x):
     return x + 1
+# 测试函数
 def test_func():
     assert func(3) == 4
 
+# 测试类
 class TestClass:
 
     def test_one(self):
         x = "this"
         assert 'h' in x
 
-    # def test_two(self):
-    #     x = "hello"
-    #     assert hasattr(x, 'check')
+    def test_two(self):
+        x = "hello"
+        assert not hasattr(x, 'check')
+
+# python内置异常树
+# BaseException
+#  +-- SystemExit
+#  +-- KeyboardInterrupt
+#  +-- GeneratorExit
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- BufferError
+#       +-- EOFError
+#       +-- ImportError
+#            +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SyntaxError
+#       |    +-- IndentationError
+#       |         +-- TabError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
+#       +-- Warning
+#            +-- DeprecationWarning
+#            +-- PendingDeprecationWarning
+#            +-- RuntimeWarning
+#            +-- SyntaxWarning
+#            +-- UserWarning
+#            +-- FutureWarning
+#            +-- ImportWarning
+#            +-- UnicodeWarning
+#            +-- BytesWarning
+#            +-- ResourceWarning
+
+# 测试异常
+def f():
+    raise SystemExit(1)
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
+
+# 除数为零异常
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
+
+# 递归深度异常
+def test_recursion_depth():
+    with pytest.raises(RuntimeError) as excinfo:
+        def f():
+            f()
+        f()
+    assert 'maximum recursion' in str(excinfo.value)
+
+# 异常信息匹配
+def myfunc():
+    raise ValueError("Exception 123 raised")
+
+def test_match():
+    with pytest.raises(ValueError) as excinfo:
+        myfunc()
+    excinfo.match(r'.* 123 .*')
+
+# 集合比较
+def test_set_comparison():
+    set1 = set("1308")
+    set2 = set("8035")
+    assert set1 != set2
+
+# 自定义比较
+class Foo:
+    def __init__(self, val):
+        self.val = val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+def test_compare():
+    f1 = Foo(1)
+    f2 = Foo(1)
+    assert f1 == f2
+
+# from test_foocompare import Foo
+def pytest_assertrepr_compare(op, left, right):
+    if isinstance(left, Foo) and isinstance(right, Foo) and op == "==":
+        return ['Comparing Foo instances:',
+                '   vals: %s != %s' % (left.val, right.val)]
+
+
+# def test_needsfiles(tmpdir):
+#     print (tmpdir)
+#     assert 0
