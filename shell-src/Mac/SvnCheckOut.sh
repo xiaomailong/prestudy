0d73ef751363623445a20755a70fb82fcb6edfa0
Bolik <Bolik@163.com>
2017-05-10 17:59:22 +0800

    轨道交通术语和定义梳理

--- shell-src/Mac/SvnCheckOut.sh
+++ shell-src/Mac/SvnCheckOut.sh
@@ -4,18 +4,23 @@
 # svn checkout http://10.201.32.43/svn/DianDongQiChe /Volumes/Work/百度云同步盘/SvnWork/DianDongQiChe
 # svn checkout http://10.201.32.43/svn/DocLib /Volumes/Work/百度云同步盘/SvnWork/DocLib
 # svn checkout http://10.201.32.43/svn/ELP-8000 /Volumes/Work/百度云同步盘/SvnWork/ELP-8000
-# svn checkout http://10.201.32.43/svn/GuZhangLuBo /Volumes/Work/百度云同步盘/SvnWork/GuZhangLuBo
+# svn checkout http://10.201.32.43/svn/EngBak /Volumes/Work/百度云同步盘/SvnWork/EngBak
 # svn checkout http://10.201.32.43/svn/GuangFuXiTong /Volumes/Work/百度云同步盘/SvnWork/GuangFuXiTong
 # svn checkout http://10.201.32.43/svn/GuiDaoJiaoTong /Volumes/Work/百度云同步盘/SvnWork/GuiDaoJiaoTong
+# svn checkout http://10.201.32.43/svn/GuZhangLuBo /Volumes/Work/百度云同步盘/SvnWork/GuZhangLuBo
 # svn checkout http://10.201.32.43/svn/KeYanGuanLi /Volumes/Work/百度云同步盘/SvnWork/KeYanGuanLi
+# svn checkout http://10.201.32.43/svn/LinuxPlat /Volumes/Work/百度云同步盘/SvnWork/LinuxPlat
 # svn checkout http://10.201.32.43/svn/PeiXunZiLiao /Volumes/Work/百度云同步盘/SvnWork/PeiXunZiLiao
+# svn checkout http://10.201.32.43/svn/PLM /Volumes/Work/百度云同步盘/SvnWork/PLM
 # svn checkout http://10.201.32.43/svn/Solar /Volumes/Work/百度云同步盘/SvnWork/Solar
 # svn checkout http://10.201.32.43/svn/Solar-8100 /Volumes/Work/百度云同步盘/SvnWork/Solar-8100
 # svn checkout http://10.201.32.43/svn/Solar-8200 /Volumes/Work/百度云同步盘/SvnWork/Solar-8200
 # svn checkout http://10.201.32.43/svn/Solar-8500 /Volumes/Work/百度云同步盘/SvnWork/Solar-8500
 # svn checkout http://10.201.32.43/svn/SolarLib /Volumes/Work/百度云同步盘/SvnWork/SolarLib
-svn checkout http://10.201.32.43/svn/SourceCode /Volumes/Work/百度云同步盘/SvnWork/SourceCode
+# svn checkout http://10.201.32.43/svn/SourceCode /Volumes/Work/百度云同步盘/SvnWork/SourceCode
 # svn checkout http://10.201.32.43/svn/TuZhiCangKu /Volumes/Work/百度云同步盘/SvnWork/TuZhiCangKu
+# svn checkout http://10.11.2.12/svn/web /Volumes/Work/百度云同步盘/SvnWork/web
 # svn checkout http://10.201.32.43/svn/WeiDianWang /Volumes/Work/百度云同步盘/SvnWork/WeiDianWang
 # svn checkout http://10.201.32.43/svn/YuanJianKu /Volumes/Work/百度云同步盘/SvnWork/YuanJianKu
 # svn checkout http://10.201.32.43/svn/iAuto8000 /Volumes/Work/百度云同步盘/SvnWork/iAuto8000
+
--- shell-src/Mac/SvnUpdate.sh
+++ shell-src/Mac/SvnUpdate.sh
@@ -7,11 +7,15 @@ svn update /Volumes/Work/百度云同步盘/SvnWork/ChangYongWenDang
 svn update /Volumes/Work/百度云同步盘/SvnWork/DianDongQiChe
 svn update /Volumes/Work/百度云同步盘/SvnWork/DocLib
 svn update /Volumes/Work/百度云同步盘/SvnWork/ELP-8000
-svn update /Volumes/Work/百度云同步盘/SvnWork/GuZhangLuBo
+svn update /Volumes/Work/百度云同步盘/SvnWork/EngBak
 svn update /Volumes/Work/百度云同步盘/SvnWork/GuangFuXiTong
 svn update /Volumes/Work/百度云同步盘/SvnWork/GuiDaoJiaoTong
+svn update /Volumes/Work/百度云同步盘/SvnWork/GuZhangLuBo
+svn update /Volumes/Work/百度云同步盘/SvnWork/iAuto8000
 svn update /Volumes/Work/百度云同步盘/SvnWork/KeYanGuanLi
+svn update /Volumes/Work/百度云同步盘/SvnWork/LinuxPlat
 svn update /Volumes/Work/百度云同步盘/SvnWork/PeiXunZiLiao
+svn update /Volumes/Work/百度云同步盘/SvnWork/PLM
 svn update /Volumes/Work/百度云同步盘/SvnWork/Solar
 svn update /Volumes/Work/百度云同步盘/SvnWork/Solar-8100
 svn update /Volumes/Work/百度云同步盘/SvnWork/Solar-8200
@@ -19,7 +23,7 @@ svn update /Volumes/Work/百度云同步盘/SvnWork/Solar-8500
 svn update /Volumes/Work/百度云同步盘/SvnWork/SolarLib
 svn update /Volumes/Work/百度云同步盘/SvnWork/SourceCode
 svn update /Volumes/Work/百度云同步盘/SvnWork/TuZhiCangKu
+svn update /Volumes/Work/百度云同步盘/SvnWork/web
 svn update /Volumes/Work/百度云同步盘/SvnWork/WeiDianWang
 svn update /Volumes/Work/百度云同步盘/SvnWork/YuanJianKu
-svn update /Volumes/Work/百度云同步盘/SvnWork/iAuto8000
 # svn update /Volumes/Work/百度云同步盘/SvnWork/bdzics8000
