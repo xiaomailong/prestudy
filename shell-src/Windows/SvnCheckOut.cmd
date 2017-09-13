79752ab7743ca0d001aca4202e39a9463c31f5fd
Bolik <Bolik@163.com>
2017-09-13 18:39:49 +0800

    openrc

--- shell-src/Windows/SvnCheckOut.cmd
+++ shell-src/Windows/SvnCheckOut.cmd
@@ -1,24 +1,24 @@
 @echo off
 
-svn checkout http://10.201.32.43/svn/ChangYongWenDang B:/SvnWork/ChangYongWenDang
-svn checkout http://10.201.32.43/svn/DianDongQiChe B:/SvnWork/DianDongQiChe
-svn checkout http://10.201.32.43/svn/DocLib B:/SvnWork/DocLib
-svn checkout http://10.201.32.43/svn/ELP-8000 B:/SvnWork/ELP-8000
-svn checkout http://10.201.32.43/svn/GuZhangLuBo B:/SvnWork/GuZhangLuBo
-svn checkout http://10.201.32.43/svn/GuangFuXiTong B:/SvnWork/GuangFuXiTong
-svn checkout http://10.201.32.43/svn/GuiDaoJiaoTong B:/SvnWork/GuiDaoJiaoTong
-svn checkout http://10.201.32.43/svn/KeYanGuanLi B:/SvnWork/KeYanGuanLi
-svn checkout http://10.201.32.43/svn/PeiXunZiLiao B:/SvnWork/PeiXunZiLiao
-svn checkout http://10.201.32.43/svn/Solar B:/SvnWork/Solar
-svn checkout http://10.201.32.43/svn/Solar-8100 B:/SvnWork/Solar-8100
-svn checkout http://10.201.32.43/svn/Solar-8200 B:/SvnWork/Solar-8200
-svn checkout http://10.201.32.43/svn/Solar-8500 B:/SvnWork/Solar-8500
-svn checkout http://10.201.32.43/svn/SolarLib B:/SvnWork/SolarLib
-svn checkout http://10.201.32.43/svn/SourceCode B:/SvnWork/SourceCode
-svn checkout http://10.201.32.43/svn/TuZhiCangKu B:/SvnWork/TuZhiCangKu
-svn checkout http://10.201.32.43/svn/WeiDianWang B:/SvnWork/WeiDianWang
-svn checkout http://10.201.32.43/svn/YuanJianKu B:/SvnWork/YuanJianKu
-svn checkout http://10.201.32.43/svn/iAuto8000 B:/SvnWork/iAuto8000
+svn checkout http://10.201.32.43/svn/ChangYongWenDang W:/SvnWork/ChangYongWenDang
+svn checkout http://10.201.32.43/svn/DianDongQiChe W:/SvnWork/DianDongQiChe
+svn checkout http://10.201.32.43/svn/DocLib W:/SvnWork/DocLib
+svn checkout http://10.201.32.43/svn/ELP-8000 W:/SvnWork/ELP-8000
+svn checkout http://10.201.32.43/svn/GuZhangLuBo W:/SvnWork/GuZhangLuBo
+svn checkout http://10.201.32.43/svn/GuangFuXiTong W:/SvnWork/GuangFuXiTong
+svn checkout http://10.201.32.43/svn/GuiDaoJiaoTong W:/SvnWork/GuiDaoJiaoTong
+svn checkout http://10.201.32.43/svn/KeYanGuanLi W:/SvnWork/KeYanGuanLi
+svn checkout http://10.201.32.43/svn/PeiXunZiLiao W:/SvnWork/PeiXunZiLiao
+svn checkout http://10.201.32.43/svn/Solar W:/SvnWork/Solar
+svn checkout http://10.201.32.43/svn/Solar-8100 W:/SvnWork/Solar-8100
+svn checkout http://10.201.32.43/svn/Solar-8200 W:/SvnWork/Solar-8200
+svn checkout http://10.201.32.43/svn/Solar-8500 W:/SvnWork/Solar-8500
+svn checkout http://10.201.32.43/svn/SolarLib W:/SvnWork/SolarLib
+svn checkout http://10.201.32.43/svn/SourceCode W:/SvnWork/SourceCode
+svn checkout http://10.201.32.43/svn/TuZhiCangKu W:/SvnWork/TuZhiCangKu
+svn checkout http://10.201.32.43/svn/WeiDianWang W:/SvnWork/WeiDianWang
+svn checkout http://10.201.32.43/svn/YuanJianKu W:/SvnWork/YuanJianKu
+svn checkout http://10.201.32.43/svn/iAuto8000 W:/SvnWork/iAuto8000
 
 echo    Press any key to exit ...
 Pause>nul
